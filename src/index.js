8cc9c6068df6daecefb14ffa7394fd0bdcd98949
Krishna <karanverma1940@gmail.com>
2026-01-31 13:53:10 +0530

    Inital commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+const PORT = 8000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to Live Commentary Server' });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
